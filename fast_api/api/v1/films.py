58613ab25614a9a7a80b40417751d76f4bfe80cf
T1rax <nikishov.v@mail.ru>
2023-03-16 13:53:38 +0400

    Part completed

--- fast_api/api/v1/films.py
+++ fast_api/api/v1/films.py
@@ -1,9 +1,37 @@
 from http import HTTPStatus
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Request
 from models.film import Film
 from services.film import FilmService, get_film_service
+import logging
+from pydantic import BaseModel
 
 router = APIRouter()
+
+logging.getLogger().setLevel(logging.INFO)
+
+# Внедряем FilmService с помощью Depends(get_film_service)
+@router.get('/', response_model=list[Film])
+async def film_query(sort: str | None = None,
+                     genre: str | None = None,
+                     page_number: int | None  = 1, 
+                     page_size: int | None  = 50, 
+                     film_service: FilmService = Depends(get_film_service)) -> Film:
+    films = await film_service.get_all_films(sort, genre, page_number, page_size)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f'Films not found')
+    return films
+
+
+# Внедряем FilmService с помощью Depends(get_film_service)
+@router.get('/search', response_model=list[Film])
+async def film_search(query: str | None  = None, 
+                      page_number: int | None  = 1, 
+                      page_size: int | None  = 50, 
+                      film_service: FilmService = Depends(get_film_service)) -> Film:
+    films = await film_service.get_by_search(q=query, page_number=page_number, page_size=page_size)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail=f'Films not found')
+    return films
 
 
 # Внедряем FilmService с помощью Depends(get_film_service)
--- fast_api/services/film.py
+++ fast_api/services/film.py
@@ -1,5 +1,6 @@
 from functools import lru_cache
 from typing import Optional
+import logging
 
 from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
@@ -11,19 +12,21 @@ from models.film import Film
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5  # 5 минут
 
+logging.getLogger().setLevel(logging.INFO)
 
 class FilmService:
     def __init__(self, redis: Redis, elastic: AsyncElasticsearch):
         self.redis = redis
         self.elastic = elastic
 
+    #Get specific film by id
     async def get_by_id(self, film_id: str) -> Optional[Film]:
-        film = None #await self._film_from_cache(film_id)
+        film = await self._film_from_cache(film_id)
         if not film:
             film = await self._get_film_from_elastic(film_id)
             if not film:
                 return None
-            #await self._put_film_to_cache(film)
+            await self._put_film_to_cache(film)
 
         return film
 
@@ -43,6 +46,66 @@ class FilmService:
         return film
 
     async def _put_film_to_cache(self, film: Film):
+        await self.redis.set(film.uuid, film.json(), FILM_CACHE_EXPIRE_IN_SECONDS)
+        
+    #All films on path /
+    async def get_all_films(self, sort: str, genre: str, page_number: int, page_size: int) -> Optional[Film]:
+        if genre is not None:
+            query = 'genre:'+genre
+        else:
+            query = '*'
+
+        films = None #await self._film_from_cache(film_id)
+        if not films:
+            films = await self._get_search_from_elastic(query=query, 
+                                                        sort=sort,
+                                                        page_number=page_number, 
+                                                        page_size=page_size)
+            if not films:
+                return None
+            #await self._put_film_to_cache(film)
+
+        return films
+
+    # Searches /seach
+    async def get_by_search(self, q: str, page_number: int, page_size: int) -> Optional[list[Film]]:
+        films = None #await self._film_from_cache(film_id)
+        if not films:
+            films = await self._get_search_from_elastic(q=q, 
+                                                        sort = None,
+                                                        page_number=page_number, 
+                                                        page_size=page_size)
+            if not films:
+                return None
+            #await self._put_film_to_cache(film)
+
+        return films
+    
+    async def _get_search_from_elastic(self, sort: str, page_number: int, page_size: int, query: str = None, q: str = None) -> Optional[list[Film]]:
+        try:
+            doc = await self.elastic.search(index='movies', 
+                                            query=query, 
+                                            q=q, 
+                                            sort=sort, 
+                                            size=page_size, 
+                                            from_=((page_number - 1)*page_size))
+        except NotFoundError:
+            return None
+        
+        data = list()
+        for item in doc['hits']['hits']:
+            data.append(Film(**item['_source']))
+        return data
+
+    async def _search_from_cache(self, query: str) -> Optional[Film]:
+        data = await self.redis.get(query)
+        if not data:
+            return None
+
+        film = Film.parse_raw(data)
+        return film
+
+    async def _put_search_to_cache(self, film: Film):
         await self.redis.set(film.uuid, film.json(), FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
