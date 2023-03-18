3bbc2b11ea5feea7cfeaaf21a8b65d79e82501b5
mod-web <module-web@yandex.ru>
2023-03-18 15:57:13 +0300

    Add description

--- fast_api/db/elastic.py
+++ fast_api/db/elastic.py
@@ -2,5 +2,5 @@ from elasticsearch import AsyncElasticsearch
 
 
 async def get_elastic() -> AsyncElasticsearch:
-    es: AsyncElasticsearch | None
+    es: AsyncElasticsearch | None = None
     return es
