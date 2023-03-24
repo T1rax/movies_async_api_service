850b9e4b8ebf1fadb273237860138aa53894a998
T1rax <nikishov.v@mail.ru>
2023-03-24 20:48:28 +0400

    fix 2

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,4 +1,4 @@
-#Imports for tests
+#Imports for local testing
 import sys
 sys.path.insert(0, '/home/tirax/movies_async_api_service/tests/functional')
 sys.path.insert(0, '/home/seo/proj/sprint_5/movies_async_api_service/tests/functional')
