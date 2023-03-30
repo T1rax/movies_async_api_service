42aec2700e38d2617e5e26166063d3f4d2a1c501
T1rax <nikishov.v@mail.ru>
2023-03-30 23:25:51 +0400

    Test config fix

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -16,8 +16,8 @@ class TestSettings(BaseSettings):
     es_url: str = Field('http://127.0.0.1:9200', env='ELASTIC_ADDRESS')
     es_id_field: str = Field('id', env='ELASTIC_ID_FIELD')
 
-    cache_host: str = Field('127.0.0.1', env='CACHE_HOST')
-    cache_port: str = Field('6379', env='CACHE_PORT')
+    cache_host: str = Field('127.0.0.1', env='API_CACHE__HOST')
+    cache_port: str = Field('6379', env='API_CACHE__PORT')
     service_url: str =Field('http://127.0.0.1:8000', env='SERVICE_ADDRESS')
 
 
